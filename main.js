401a5bbba14dd653094b15c59490c7749691d7fb
keenzman <szaman_03@hotmail.co.uk>
2021-05-27 09:56:18 +0100

    display 3x3 grid

---
+++ main.js
@@ -0,0 +1,10 @@
+// 3x3 grid
+// UI -> what the user will use
+// HTML, CSS grid
+// getting grid on square
+
+// Connect 3 to win -> three of a set -> horizontal, vertical, diagonal
+// 2 players -> 0s and Xs
+
+// 0s and Xs -> connect three to win
+// players turn -> click on square to make it either X/0
